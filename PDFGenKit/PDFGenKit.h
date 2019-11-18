63aa4aa640c80c37c7c3dd73a3bd8e466d6dc614
Max Blanchard <blanchard.mx@gmail.com>
2019-11-17 23:01:41 -0500

    Initial Commit

---
+++ PDFGenKit/PDFGenKit.h
@@ -0,0 +1,19 @@
+//
+//  PDFGenKit.h
+//  PDFGenKit
+//
+//  Created by Maxime Blanchard on 2019-11-17.
+//  Copyright © 2019 Maxime Blanchard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PDFGenKit.
+FOUNDATION_EXPORT double PDFGenKitVersionNumber;
+
+//! Project version string for PDFGenKit.
+FOUNDATION_EXPORT const unsigned char PDFGenKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDFGenKit/PublicHeader.h>
+
+
---
+++ PDFGenKitExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PDFGenKitExamples
+//
+//  Created by Maxime Blanchard on 2019-11-17.
+//  Copyright © 2019 Maxime Blanchard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
